3099f0ebe6a84366d0f5fed60a94352e11fe14b3
Ilias Kouroudis <ilias@rocking.gr>
2018-04-04 09:59:01 +0300

    Initial commit of new files (original script)

---
+++ plugin.rb
@@ -0,0 +1,113 @@
+# name: discourse-adplugin
+# about: Ad Plugin for Discourse
+# version: 1.0.2
+# authors: Vi and Sarah (@ladydanger and @cyberkoi)
+# url: https://github.com/discourse/discourse-adplugin
+
+register_css <<CSS
+
+.google-dfp-ad {
+  padding: 3px 0;
+  margin-bottom: 10px;
+  clear: both;
+}
+
+.google-dfp-ad  .dfp-ad-unit {
+  margin: 0 auto;
+}
+
+.google-adsense {
+  padding: 3px 0;
+  margin-bottom: 10px;
+  clear: both;
+}
+
+.google-adsense.adsense-responsive {
+  width: 100%;
+}
+
+.google-adsense .google-adsense-label {
+  width: 728px;
+  margin: 0 auto;
+}
+
+.google-adsense.adsense-responsive .google-adsense-label {
+  width: 100%;
+  text-align: center;
+}
+
+.google-adsense .adsense-unit {
+  margin: 0 auto;
+}
+
+.google-adsense .google-adsense-label h2 {
+  margin: 4px 0 !important;
+  color: #858a8c;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: normal;
+}
+
+.google-adsense .google-adsense-content {
+  margin: 0 auto;
+}
+
+.google-adsense.adsense-post-bottom {
+  max-width: 735px;
+  padding: 0 11px;
+}
+
+@media all
+and (max-width : 775px) {
+  .google-adsense.adsense-post-bottom {
+    box-sizing: border-box;
+    width: 100%;
+  }
+}
+
+.amazon-product-links {
+  padding: 3px;
+  margin-bottom: 10px;
+  clear: both;
+}
+
+.amazon-product-links  .amazon-unit {
+  margin: 0 auto;
+}
+
+.amazon-product-links .amazon-product-links-label {
+  width: 728px;
+  margin: 0 auto;
+}
+
+.amazon-product-links .amazon-product-links-label h2 {
+  margin: 4px 0 !important;
+  color: #858a8c;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: normal;
+}
+
+.google-dfp-ad .google-dfp-ad-label {
+  width: 728px;
+  margin: 0 auto;
+}
+
+.google-dfp-ad .google-dfp-ad-label h2 {
+  margin: 4px 0 !important;
+  color: #858a8c;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: normal;
+}
+
+.google-dfp-ad.dfp-ad-post-bottom {
+  .google-dfp-ad-label, .dfp-ad-unit {
+    margin: 0 0 0 52px;
+  }
+}
+
+CSS
